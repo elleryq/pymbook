b2b859a663342ea8f8171adc72c725b92e2d3dd2
elleryq <elleryq@gmail.com>
2010-09-02 09:07:01 +0000

    Update license and move PDBContents out of pymbook.py.

--- pymbooklib/pdbwidget.py
+++ pymbooklib/pdbwidget.py
@@ -15,7 +15,9 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#  along with pymbook.  If not, see <http://www.gnu.org/licenses/>.
+
+"""PDBWidget"""
 import gtk
 
 class PDBWidget(gtk.DrawingArea):
