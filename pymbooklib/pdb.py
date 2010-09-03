7b33970feb595e4abf131a42598eebe4b0c747fd
elleryq <elleryq@gmail.com>
2010-09-03 03:13:43 +0000

    Add license comments.

--- pymbooklib/pdb.py
+++ pymbooklib/pdb.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Metadata-Version: 1.1
-Name: pdb
-Version: 0.1
-Platform: POSIX, Windows
-Summary: A module for read pdb files from http://www.haodoo.net
-Description: The specification is in http://www.haodoo.net/?M=hd&P=mPDB22#P
-             PDBFile is the implementation for reading pdb files.
-Keywords: pdb haodoo
-Home-page: http://code.google.com/p/pymbook/
-Authors: Yan-ren Tsai
-Author-email: "Yan-ren Tsai" <elleryq@gmail.com>
-License: GNU General Public License v3
-Requires: sys
-Requires: os
-Provides: PDBFile
-Provides: PDBException
-"""
-
+#
+#  Copyright (C) 2010 elleryq@gmail.com
+#
+#  This file is part of pymbook
+#  pymbook is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pymbook is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with pymbook.  If not, see <http://www.gnu.org/licenses/>.
+"""PDBFile"""
 import sys
 import os
 import locale
