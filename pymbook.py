b3863002ea848f7ba15dd5fcfd883905537940aa
elleryq <elleryq@gmail.com>
2013-07-28 09:30:11 +0000

    Remove comments in TabWidget.

--- pymbook.py
+++ pymbook.py
@@ -76,15 +76,8 @@ class StateMachine(object):
 class TabWidget(QTabWidget):
     def __init__(self, parent=None):
         super(TabWidget, self).__init__(parent)
-        #self.setTabsClosable(True)
-        #self.tabCloseRequested.connect(self.removeTab)
+        self.setTabsClosable(False)
         self.tabBar().setVisible(False)
-
-    #def tabInserted(self, index):
-    #    self.tabBar().setVisible(self.count() > 1)
-
-    #def tabRemoved(self, index):
-    #    self.tabBar().setVisible(self.count() > 1)
 
 
 class MainWindow(QMainWindow):
